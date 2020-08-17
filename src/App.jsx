79ae0fa0a7a318cf01d5b6dd623e5e7533fb1f1b
Abhideep Maity <abhideepm@gmail.com>
2020-08-17 13:53:28 +0530

    integration with react done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
-	return <div></div>
+	return (
+		<div>
+			<h1 className="text-center">Video Converter</h1>
+		</div>
+	)
 }
 
 export default App
